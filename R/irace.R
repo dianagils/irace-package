a384b081355f1e70f966bb30ea476948a6346557
dianagils <56230661+dianagils@users.noreply.github.com>
2024-05-19 17:09:40 -0400

    nrow

--- R/irace.R
+++ R/irace.R
@@ -1466,7 +1466,7 @@ irace_run <- function(scenario, parameters)
     # Add instances if needed
     # Calculate budget needed for old instances assuming non elitist irace
     cat('irace results')
-    print(iraceResults$experiments)
+    print(nrow(iraceResults$experiments[[as.character(subset_number)]]))
     for (subset_number in unique(subsets$SubsetNumber)) {
       currentSubset <- subsets[subsets$SubsetNumber == subset_number]
       currentSubset$NextInstance <- nrow(iraceResults$experiments[[as.character(subset_number)]]) + 1
