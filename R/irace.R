2a163911523da43c3ee911e19acb8acfc8a45db5
dianagils <56230661+dianagils@users.noreply.github.com>
2024-05-20 20:14:37 -0400

    subset result

--- R/irace.R
+++ R/irace.R
@@ -1520,7 +1520,7 @@ irace_run <- function(scenario, parameters)
                                         cbind(rep(indexIteration, nrow(raceResults$experimentLog)),
                                               raceResults$experimentLog))
     
-    for (iraceResults$experiments[[subset_number]]subset_number in unique(subsets$SubsetNumber)) {
+    for (subset_number in unique(subsets$SubsetNumber)) {
       subsetResults <- raceResults$experiments[[as.character(subset_number)]]
       iraceResults$experiments[[subset_number]] <- merge.matrix (iraceResults$experiments[[subset_number]],
                                               subsetResults)
