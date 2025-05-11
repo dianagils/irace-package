1b8229b6b29cb15e3a8a9623e25ec1c4ca3856bf
d-gils <dgil@systep.cl>
2025-05-11 15:11:34 -0400

    Refactor checkScenario function to simplify parameter printing

--- R/readConfiguration.R
+++ R/readConfiguration.R
@@ -439,8 +439,8 @@ checkScenario <- function(scenario = defaultScenario())
 {
   quote.param <- function(name)
   {
-    print(name)
-    print(" (", .irace.params.def[name, "long"], ")")
+    print(.irace.params.def[name, ])
+    print(.irace.params.def[name, "long"])
 
     if (.irace.params.def[name, "long"] != "") {
       return(paste0("'", name, "' (", .irace.params.def[name, "long"], ")"))
