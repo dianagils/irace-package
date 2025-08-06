74534c7950952961c7dcc5b703b0873f745b85cb
d-gils <dgil@systep.cl>
2025-08-06 16:38:25 -0400

    fix irace_run function to correctly assert experiments used in subset handling

--- R/irace.R
+++ R/irace.R
@@ -1189,7 +1189,7 @@ irace_run <- function(scenario, parameters)
       # Extract the subset rows
       currentSubset <- subsets[current_indices, ]
       if (scenario$elitist) {
-        irace.assert(sum(!is.na(iraceResults$experiments[[subsetNumber]])) == currentSubset$experimentsUsedSoFar)
+        irace.assert(sum(!is.na(iraceResults$experiments[[subsetNumber]])) == (currentSubset$experimentsUsedSoFar + currentSubset$experimentsUsed))
       }
       # Check the conditions
       if (any(currentSubset$remainingBudget <= 0) || (scenario$maxTime > 0 && any(currentSubset$timeUsed >= scenario$maxTime))) {
