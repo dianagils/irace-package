ad39c12c32807a384d72291d5420cb13416ba079
d-gils <dgil@systep.cl>
2025-10-19 15:50:39 -0300

    try to fix the NA situation

--- R/race.R
+++ R/race.R
@@ -830,15 +830,19 @@ elitist_race <- function(maxExp = 0,
   # Initialize a list to store matrices for results separated by subsets
   result_list <- list()
 
+  print("Unique SubsetNumber values:")
+  print(unique(subset.data$SubsetNumber))
+  print("Structure:")
+  str(subset.data)
+  print("Any NAs?")
+  print(any(is.na(subset.data$SubsetNumber)))
+
+
   # Iterate over each subset
   for (subset_number in unique(subset.data$SubsetNumber)) {
-    print(subset_number)
+    if (is.na(subset_number)) next 
     indexes <- sapply(configurations$isAliveInSubset, function(lst) subset_number %in% lst)
     subset_configs <- configurations[indexes,]
-    print('subset configs: ')
-    print(subset_configs)
-
-    print('elite safe per subset: ')
     print(elite.safe_per_subset[[as.character(subset_number)]])
     # Attempt to create the matrix
     subset_results <- matrix(NA, 
