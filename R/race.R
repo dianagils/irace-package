000d9bb671b90cdb9d59bd067650148c1deb86f5
d-gils <dgil@systep.cl>
2025-10-26 12:46:49 -0300

    fix the creation of the rank col

--- R/race.R
+++ R/race.R
@@ -1492,10 +1492,7 @@ elitist_race <- function(maxExp = 0,
   alivePerSubset <- vector("list", length(unique_subset_numbers))
   names(alivePerSubset) <- unique_subset_numbers
   configurations_copy <- configurations 
-  for (subset_number in unique_subset_numbers) {
-    rank_col <- paste0(".RANK_", subset_number)
-    configurations[[rank_col]] <- vector("list", nrow(configurations))
-  }
+  configurations[[rank_col]] <- Inf
   configurations$isAliveInSubset <- lapply(seq_len(nrow(configurations)), function(i) { list() })
 
   for (subset_number in unique_subset_numbers) {
