dd8b29a3cc2f5d9c88fb1e1d783977726e9a5368
d-gils <dgil@systep.cl>
2025-11-08 13:55:03 -0300

    fix the assert

--- R/race.R
+++ R/race.R
@@ -780,8 +780,9 @@ elitist_race <- function(maxExp = 0,
     subset.data[subset_number,]$experimentsUsed <- 0
     # Subset elite data for the current subset
     elite_data_subset <- elite.data[[as.character(subset_number)]]
+    print(elite_data_subset)
     next_instance <- subset.data[subset.data$SubsetNumber == subset_number, "NextInstance"]
-    
+    print(next_instance)
     # Check if elite data is NULL
     if (is.null(elite_data_subset)) {
       # If elite data is NULL, set elite.safe to elitistNewInstances and elite.instances.ID to NULL
